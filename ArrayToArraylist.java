4eefadb9da6b3f2a04ad76aa38de81a9ab3cdf93
Anurag Anand <anuraganand448@gmail.com>
2023-01-28 10:57:01 +0530

    Add files via upload

---
+++ ArrayToArraylist.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class ArrayToArraylist
+{
+	public static void main (String[] args)
+	{
+		String[] arr = {"Rahul", "Utkarsh",
+						"Shubham", "Neelam"};
+
+		
+		List al = Arrays.asList(arr);
+
+		System.out.println(al);
+	}
+}
---
+++ SecondSmall.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+import java.util.*;
+
+class SecondSmall {
+
+public static void main(String args[])
+{
+	int arr[]={111, 13, 25, 9, 34, 1};
+	int n=arr.length;
+
+	
+	Arrays.sort(arr);
+
+	
+	System.out.println("second smallest element is "+arr[1]);
+}
+}
+
+
